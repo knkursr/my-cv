d9bdd69d02249b8a72f4572de43c3af2b6bd34e4
knkursr <croakker@gmail.com>
2025-01-08 00:09:36 +0100

    done

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,62 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './style.scss'
+
+interface Experience {
+  year: number;
+  description: string;
+}
+
+interface CVData {
+  photo: string;
+  name: string;
+  lastName: string;
+  position: string;
+  experience: Experience[]
+  education: string[];
+}
+
+const cvData: CVData = {
+  photo: "https://i.imgur.com/WP5QRlF.jpeg",
+  name: "Kris",
+  lastName: "Nar",
+  position: "student",
+  experience: [
+    { year: 2020, description: "Lorem ipsum"},
+    { year: 2022, description: "Iprem losum"}
+  ],
+  education: [
+    "University of Lorem Ipsum",
+    "University of Dolor sit amet"
+  ]
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <main className='cv-main'>
+      <header className='cv-header'>
+        <h1>CV {cvData.name} {cvData.lastName}</h1>
+      </header>
+      <aside className='cv-personal'>
+        <h2>Personal data</h2>
+        <img className='cv-photo' src={cvData.photo} alt={`${cvData.name} ${cvData.lastName} photo`} />
+        <p>{cvData.name} {cvData.lastName}</p>
+        <small>{cvData.position}</small>
+      </aside>
+      <section className='cv-details'>
+        <h2>Experience</h2>
+        <ul>
+          {cvData.experience.map(experience => (
+            <li key={experience.year}>
+              <strong>{experience.year}</strong> - {experience.description}
+            </li>
+          )) }
+        </ul>
+        <h2>Education</h2>
+        <ul>
+          {cvData.education.map((education, index) => <li key={index}>{education}</li>)}
+        </ul>
+      </section>
+    </main>
   </StrictMode>,
 )
